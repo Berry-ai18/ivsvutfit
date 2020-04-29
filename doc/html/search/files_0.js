7c16e9265fde79dce2492d91fe99f7fccc81f05f
VladimiraEstragon <andrej.hyros@icloud.com>
2020-04-30 01:02:21 +0200

    ivskalkv01 -> doc

---
+++ doc/html/search/files_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['form1_2ecs',['Form1.cs',['../Form1_8cs.html',1,'']]],
+  ['form1_2edesigner_2ecs',['Form1.Designer.cs',['../Form1_8Designer_8cs.html',1,'']]]
+];
---
+++ doc/html/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['form1',['Form1',['../classIVS__Calc__v01_1_1Form1.html#a87ab83358980f9041a611a2a809fd991',1,'IVS_Calc_v01::Form1']]]
+];
